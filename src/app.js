9c2531a987bb0205137fb17dfd0d4e4f3be052a5
lucassoares02 <lucas.soares02@hotmail.com>
2026-03-15 14:33:11 -0300

    fix dotenv in server

--- src/app.js
+++ src/app.js
@@ -8,6 +8,5 @@ app.use(cors({ origin: "*", allowedHeaders: "X-Requested-With, Content-Type, use
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", routes);
-("");
 
 module.exports = app;
